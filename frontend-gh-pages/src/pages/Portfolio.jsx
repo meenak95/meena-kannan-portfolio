e2d575a658e0d150ee8745b530964591ce72c959
CI <ci@example.com>
2025-08-30 04:59:09 +0000

    Import Link from react-router-dom in Portfolio component
    Co-authored-by: meenakannan92 <meenakannan92@gmail.com>

--- frontend-gh-pages/src/pages/Portfolio.jsx
+++ frontend-gh-pages/src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 // import SciFiBackground from '../components/SciFiBackground'
 import ProjectCard from '../components/ProjectCard'
 import {
